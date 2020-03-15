3fab5bfd6d57e59498f9e3079af3a62f0e1fea65
Charlotta Fruechtenicht <charlotta.fruechtenicht@roche.com>
2020-03-15 08:29:47 +0100

    Convienence function for creating table 1

--- R/vr_table_one.R
+++ R/vr_table_one.R
@@ -1,9 +1,21 @@
 vr_table_one <- function(
     data, 
+    title, 
+    dataset,
+    caption = "",
+    subtitle = "",
     abbreviations = "", 
     variable_definitions = "", 
+    groupCols = NULL,
+    summary_function = vr_summarize,
     ...
 ) {
-    # TODO: Charlotta to implement this
-    # See vr_attrition for an example of documentation strings
+    # create table
+    tab1_df <- vr_create_tableone(data, groupCols = groupCols, summary_function = summary_function)
+    
+    # render table
+    tab1_rendered <- tab1_df %>% 
+        vr_render_tableone(title = title, subtitle = subtitle, caption = caption, datasource = dataset, ...) %>% 
+    
+    return(tab1_rendered)
 }
