d4a884729de7caff7327a600810b9ca0f739163c
bailliem <bailliem@gmail.com>
2020-03-23 11:21:02 +0100

    adding tmp files

--- R/survminer-ggplot.R
+++ R/survminer-ggplot.R
@@ -3,3 +3,13 @@ library(survminer)
 model <- coxph( Surv(time, status) ~ sex + rx + adhere,
                 data = colon )
 ggforest(model)
+
+colon <- within(colon, {
+  sex <- factor(sex, labels = c("female", "male"))
+  differ <- factor(differ, labels = c("well", "moderate", "poor"))
+  extent <- factor(extent, labels = c("submuc.", "muscle", "serosa", "contig."))
+})
+bigmodel <-
+  coxph(Surv(time, status) ~ sex + rx + adhere + differ + extent + node4,
+        data = colon )
+ggforest(bigmodel)
