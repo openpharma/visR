776e3842c865bddad384d93323362c4e7fb91663
bailliem <bailliem@gmail.com>
2020-03-24 09:44:06 +0100

    lint code

--- R/vr_kaplan_meier_summary.R
+++ R/vr_kaplan_meier_summary.R
@@ -31,19 +31,19 @@
 vr_kaplan_meier_summary <- function(data, equation) {
 
 # Run survival function
-fit <- survival::survfit(eval(parse(text=equation)), data = data)
+fit <- survival::survfit(eval(parse(text = equation)), data = data)
 
 # Summary Table with persons at risk, events, median survival times along with 95% CIs over strata
 median_survival_time_summary <- 
-  dplyr::as_tibble(summary(fit)$table, rownames="strata") %>%
-  dplyr::mutate(strata=sub(".*=", "", names(fit$strata))) %>%
+  dplyr::as_tibble(summary(fit)$table, rownames = "strata") %>%
+  dplyr::mutate(strata = sub(".*=", "", names(fit$strata))) %>%
   dplyr::select(strata,records,events,median,'0.95LCL','0.95UCL') %>%
-  dplyr::rename('# persons'=records, '# events'=events, 'median survival time'=median)
+  dplyr::rename('# persons' = records, '# events' = events, 'median survival time' = median)
   
 # test of equality over strata
-log_rank    <- cbind(Test='Log-rank',    broom::glance(survdiff(eval(parse(text=equation)), data=data, rho=0)), Description='Log-rank gives more weight on higher values of time', stringsAsFactors=F)
-wilcoxon    <- cbind(Test='Wilcoxon',    broom::glance(survdiff(eval(parse(text=equation)), data=data, rho=1)),   Description='Wilcoxon gives more weight on lower values of time', stringsAsFactors=F)
-tarone_ware <- cbind(Test='Tarone-Ware', broom::glance(survdiff(eval(parse(text=equation)), data=data, rho=1.5)), Description='Tarone-Ware is in between', stringsAsFactors=F)
+log_rank    <- cbind(Test = 'Log-rank',    broom::glance(survdiff(eval(parse(text = equation)), data = data, rho = 0)), Description = 'Log-rank gives more weight on higher values of time', stringsAsFactors = F)
+wilcoxon    <- cbind(Test = 'Wilcoxon',    broom::glance(survdiff(eval(parse(text = equation)), data = data, rho = 1)),   Description = 'Wilcoxon gives more weight on lower values of time', stringsAsFactors = F)
+tarone_ware <- cbind(Test = 'Tarone-Ware', broom::glance(survdiff(eval(parse(text = equation)), data = data, rho = 1.5)), Description = 'Tarone-Ware is in between', stringsAsFactors = F)
 
 # Summary table with test of equality over strata
 equality_of_strata <- 
