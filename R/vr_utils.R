8241f9449485e82a74a89b7255f1aad7f964669a
epijim <james@epijim.uk>
2020-05-17 00:12:00 +0200

    return specified with no content

--- R/vr_utils.R
+++ R/vr_utils.R
@@ -3,13 +3,12 @@ vr_summarize <- function(x) UseMethod("vr_summarize")
 
 #' Create variable summary for factors
 #'
-#' @param x 
+#' @param x
 #'
-#' @return
 #' @export
 vr_summarize.factor <- function(x){
   x1 <- forcats::fct_explicit_na(x, na_level = "Missing")
-  
+
   dat <- tibble::enframe(x1) %>%
     dplyr::group_by(value) %>%
     dplyr::summarise(N = n()) %>%
@@ -21,9 +20,8 @@ vr_summarize.factor <- function(x){
 
 #' Create variable summary for numeric variables
 #'
-#' @param x 
+#' @param x
 #'
-#' @return 
 #' @export
 vr_summarize.numeric <- function(x){
   dat <- list(
@@ -40,9 +38,8 @@ vr_summarize.numeric <- function(x){
 
 #' Create variable summary for all other variable types
 #'
-#' @param x 
+#' @param x
 #'
-#' @return
 #' @export
 vr_summarize.default <- function(x){
   dat <- list(
@@ -57,18 +54,17 @@ vr_summarize_tab1 <- function(x) UseMethod("vr_summarize_tab1")
 
 #' Create variable summary for factors
 #'
-#' @param x 
+#' @param x
 #'
-#' @return
 #' @export
 vr_summarize_tab1.factor <- function(x){
   x1 <- forcats::fct_explicit_na(x, na_level = "Missing")
-  
+
   dat <- tibble::enframe(x1) %>%
     dplyr::group_by(value) %>%
     dplyr::summarise(N = n()) %>%
     dplyr::mutate(`n (%)` = paste0(N, " (", format(100 * N/sum(N), digits = 3, trim=TRUE), "%)")) %>%
-    dplyr::select(-N) %>% 
+    dplyr::select(-N) %>%
     tidyr::pivot_wider(names_from = value, values_from = c("n (%)"), names_sep=" ") %>%
     as.list()
   list(dat)
@@ -76,14 +72,13 @@ vr_summarize_tab1.factor <- function(x){
 
 #' Create variable summary for numeric variables
 #'
-#' @param x 
+#' @param x
 #'
-#' @return 
 #' @export
 vr_summarize_tab1.numeric <- function(x){
   dat <- list(
     `Mean (SD)` = paste0(format(mean(x, na.rm = T), digits = 3), " (", format(sd(x, na.rm = T), digits = 3), ")"),
-    `Median (IQR)` = paste0(format(median(x, na.rm = T)), " (", format(quantile(x, probs=0.25)), 
+    `Median (IQR)` = paste0(format(median(x, na.rm = T)), " (", format(quantile(x, probs=0.25)),
                             "-", format(quantile(x, probs=0.75)), ")"),
     `Min-max` = paste0(format(min(x, na.rm = T)), "-", format(max(x, na.rm = T))),
     Missing = paste0(format(sum(is.na(x))), " (", format(100 * sum(is.na(x))/n(), trim=TRUE), "%)")
@@ -93,9 +88,8 @@ vr_summarize_tab1.numeric <- function(x){
 
 #' Create variable summary for all other variable types
 #'
-#' @param x 
+#' @param x
 #'
-#' @return
 #' @export
 vr_summarize_tab1.default <- function(x){
   dat <- list(
@@ -107,9 +101,8 @@ vr_summarize_tab1.default <- function(x){
 
 #' Create a caption for any table
 #'
-#' @param x 
+#' @param x
 #'
-#' @return 
 #' @export
 vr_table_caption <-function(caption,
          label = knitr::opts_current$get("label"),
@@ -126,27 +119,26 @@ vr_table_caption <-function(caption,
 
 #' Create a caption for any table
 #'
-#' @param x 
+#' @param x
 #'
-#' @return 
 #' @export
 vr_table_download <- function(df, filename=NULL, format="csv", button="Download data"){
-  
+
   if(!format %in% c("txt", "tsv", "csv", "xlsx")){
     stop("Output file format has to be either txt, tsv, csv or xlsx.")
   }
-  
+
   if(is.null(knitr::opts_knit$get("fig.path"))){
     fp <- tempdir()
   }
   else{
     fp <- knitr::opts_knit$get("fig.path")
   }
-  
+
   if(is.null(filename)){
     filename <- tempfile(pattern = paste("data-"), tmpdir = getwd(), fileext = paste0(".", format))
   }
-  
+
   if(format %in% c("csv", "tsv", "txt")){
     delim <- switch(format, "csv" = ",", "tsv" = "\t", "txt" = " ")
     write.table(df, file = filename, sep = delim)
@@ -154,7 +146,7 @@ vr_table_download <- function(df, filename=NULL, format="csv", button="Download 
   else{
     xlsx::write.xlsx(df, file=filename)
   }
-  
+
   # String result ready to be placed in rmarkdown
   asis_output(paste0('<a class="btn btn-info" role="button" download="', filename, '" href="', filename, '">', button, '</a>'))
   # paste0('<button type="submit" onclick="window.open("', filename, '")">', button, '</button>')
