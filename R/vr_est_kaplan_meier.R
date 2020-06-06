90dd80ba3ad274a118fa6a94a7d268ff43c8d26c
shaesen2 <haesendonckx.steven@gmail.com>
2020-06-06 14:28:26 +0200

    vr_plt_KM:  MOre general proposal
    vr_est_KM:
     Allow for one stratum

--- R/vr_est_kaplan_meier.R
+++ R/vr_est_kaplan_meier.R
@@ -1,6 +1,6 @@
-#' Tidy a Kaplan-Meier survfit object
+#' @title Tidy a Kaplan-Meier survfit object
 #'
-#' This function tidies a Kaplan-Meier Surv object using broom tidy function. 
+#' @description This function tidies a Kaplan-Meier Surv object using broom tidy function for downstream processing. 
 #' The function expects a data frame, tibble or data.table and the equation for the survival model. 
 #'
 #' @param data input data frame, tibble or data.table for the Kaplan-Meier analysis. 
@@ -10,6 +10,7 @@
 #' @export
 #'
 #' @examples
+#' library(visR)
 #' library(survival)
 #' 
 #' ## No stratification
@@ -17,10 +18,15 @@
 #' 
 #' ## Stratified Kaplan-Meier analysis by `sex`
 #' vr_est_kaplan_meier(data = lung, equation = "Surv(time, status) ~ sex")
+#' 
+#' ## Stratification with one level
+#' 
+#' df <- lung
+#' df$blah <- "One level present"
+#' vr_est_kaplan_meier(data = df, equation = "Surv(time, status) ~ blah")
 
- 
+
 vr_est_kaplan_meier <- function(data, equation) {
-  
   data <- as.data.frame(data)
   
   survfit_object <- survival::survfit(
@@ -30,9 +36,24 @@ vr_est_kaplan_meier <- function(data, equation) {
   # No strata: Create an artificial one for compatibility with downstream processing
   if (is.null(survfit_object$strata)){
     survfit_object$strata <- as.vector(length(survfit_object$time))
-    attr(survfit_object$strata, "names") <- "Overall"
+    
+    strat_var <- base::trimws(base::sub('.*~', '', base::gsub(' ', '', equation)), which="both")
+    
+    if (strat_var == "1"){
+      # overall analysis
+      attr(survfit_object$strata, "names") <- "Overall"
+    } else {
+      # ~ x with One level in variable present
+      attr(survfit_object$strata, "names") <- data[1, strat_var]
+    }
   } 
 
   broom_object <- broom::tidy(survfit_object)
   return(broom_object)
 }
+
+### Ideas for improvement
+ ## rename function vr_KM_estimation
+ ## Use of CNSR AVAL CHG. Only information required is input df and main effect (overall = 1, TRT01P, ..)
+
+
--- R/vr_plt_kaplan_meier.R
+++ R/vr_plt_kaplan_meier.R
@@ -3,91 +3,107 @@
 #' TODO: Define and describe the purpose of this function. 
 #' 
 #' @param broom_object, 
-#' @param title = "", 
-#' @param abbreviations = NULL, 
-#' @param variable_definitions = NULL, 
-#' @param N = NULL, 
-#' @param N_unit = "patients", 
-#' @param time_unit = "days", 
-#' @param data_source = NULL, 
-#' @param estimate_name = "survival probability" 
 #'
 #' @return ggplot object 
 #' @export
 #'
 #' @examples
 #' # TODO: Define an example for this function
-#' # Create interim data models
+#' library(visR)
+#' library(dplyr)
 #' library(survival)
 #' library(ggplot2)
-#' library(dplyr)
+#' 
 #' data("veteran")
 #' data <-  veteran %>% 
 #'     mutate(trt = as.factor(case_when(
 #'        trt == 1 ~ "standard therapy", 
 #'        trt == 2 ~ "test chemotherapy"
-#'    )))
+#'        ))
+#'        , blah = "Only level")
 #'
-#' equation <- "survival::Surv(time, status) ~ trt"
-#' risk_table_data <- vr_est_km_risk_table(data, equation)
-#' time_unit = "days"
-#' broom_object <- vr_est_kaplan_meier(data, equation)
-#' risk_table_data <- vr_est_km_risk_table(data, equation)
-#' 
-#' # Create plot
-#' table <- vr_plt_km_risk_table(risk_table_data,
-#'                               time_unit = time_unit)
-#'                               
-#' ##not run TODO: Check why this example is failing
-#' vr_plt_kaplan_meier(
-#'  broom_object, 
-#'  N = "Patients", 
-#'  time_unit = "days", 
-#'  data_source = "this is the data source label"
-#')
-vr_plt_kaplan_meier <- function(
-    broom_object, 
-    title = "", 
-    abbreviations = NULL, 
-    variable_definitions = NULL, 
-    N = NULL, 
-    N_unit = "patients", 
-    time_unit = "days", 
-    data_source = NULL, 
-    estimate_name = "survival probability"
-) {
-    
-    # Get number of patients
-    if (is.null(N)) {
-        N <- 
-            broom_object  %>% 
-            dplyr::group_by(strata) %>% 
-            dplyr::filter(row_number() == 1) %>% 
-            dplyr::ungroup() %>% 
-            dplyr::summarize(N = sum(n.risk)) %>% 
-            dplyr::pull(N)
-    }
+#' vr_KM_plot(
+#'     broom_object = vr_est_kaplan_meier(data, "survival::Surv(time, status) ~ trt")
+#'    ,title = "Time to First Dermatologic Event"
+#'    ,y_label = "Suvival Probability"
+#'    ,x_label = NULL                    
+#'    ,xaxistable=FALSE
+#'    ,conf_limits = T
+#'  )
+#'  
+#' vr_KM_plot(
+#'     broom_object = vr_est_kaplan_meier(data, "survival::Surv(time, status) ~ blah")
+#'    ,title = "Time to First Dermatologic Event"
+#'    ,y_label = "Suvival Probability"
+#'    ,x_label = NULL                    
+#'    ,xaxistable=FALSE
+#'    ,conf_limits = T
+#'  )
 
-    # Build caption
-    data_source_caption <- sprintf("Data Source: %s", data_source)
-    abbreviations_caption <- sprintf("Abbreviations: %s", abbreviations)
-    variable_definitions_caption <- sprintf("Variable Definitions: %s", 
-        variable_definitions)
+
+
+## minimal proposal
+  vr_KM_plot <- function(
+     broom_object = NULL                # object: does it need to be broom? Can we input KM object instead? (keeping in mind validation and possible breaking updates in broom)
+    ,title = NULL
+    ,subtitle=NULL               
+    ,conf_limits=FALSE                  # Alpha level needs to be taken care off in estimation function
+    ,y_label = "Suvival Probability"
+    ,x_label = "time"                   # take PARAMCD if present in broom object => use label of AVAL/CHG, ... Time (days) is not sufficient We would need "Time to resolution of xxx"
+    ,xaxistable=FALSE
+    ,theme=NULL
+  ){
+
+  #validation and verification: eg are we dealing with tidy object or list of objects?
     
-    plot <- ggplot2::ggplot(broom_object, aes(x = time)) + 
-        pammtools::geom_stepribbon(aes(ymin = conf.low, ymax = conf.high, fill = strata), 
-            alpha = 0.25) + 
-        geom_step(aes(y = estimate, col = strata)) + 
-        # ggplot2::theme_light() + 
-        ggsci::scale_color_nejm() + 
-        ggsci::scale_fill_nejm() + 
-        ggplot2::ylab(estimate_name) + 
-        ggplot2::xlab(sprintf("time (%s)", time_unit)) + 
-        ggplot2::labs(
-            title = title, 
-            subtitle = sprintf("N [%s] = %s", N_unit, N), 
-            caption = paste(abbreviations_caption, 
-                variable_definitions_caption, data_source_caption))
+  #obtain alternative defaults such as xaxislabel, title, subtitle
+    
+  #look for defined theme inside global environment, if not present look for theme argument, otherwise use bw
+    
+    plot <- ggplot2::ggplot(broom_object, aes(x = time)) +
+      geom_step(aes(y = estimate, col = strata)) + 
+      ggsci::scale_color_nejm() + 
+      ggsci::scale_fill_nejm() + 
+      ggplot2::ylab(y_label) + 
+      ggplot2::xlab(sprintf("%s", x_label)) + 
+      NULL
       
+    if (conf_limits == T){
+      plot <- plot + pammtools::geom_stepribbon(aes(ymin = conf.low, ymax = conf.high, fill = strata), alpha = 0.25)
+    }
+    
+    if (!is.null(title)){
+      plot <- plot +  ggplot2::labs(title = title)
+    } 
+    
+    if (!is.null(subtitle)){
+      plot <- plot +  ggplot2::labs(subtitle = subtitle)
+    } 
+    
+    ## add theme to graph
+    if (is.null(theme) & exists("theme", envir = .GlobalEnv) & !is.null(get("theme", envir = .GlobalEnv)) ){
+      plot <- plot + theme
+    } else if (is.null(theme) & (!exists("theme", envir = .GlobalEnv) | is.null(get("theme", envir = .GlobalEnv)))) {
+      plot <- plot +  ggplot2::theme_bw()
+    } else if (!is.null(theme)){
+      #catherror to see if exist
+      plot <- plot + theme
+    } else {
+      warning("Variable 'theme' can't be found or non-conform to ggplot2 guidelines. Function will apply default theme_bw.")
+      plot <- plot +  ggplot2::theme_bw()
+    }
+  
+    
+
+    ## legend position
+    
+    
+    ## Risk table
+    
+    
+    ## ggrob everything into grid
+ 
+
+    
     return(plot)
 }
