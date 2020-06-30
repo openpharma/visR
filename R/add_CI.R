30cc041eae3f018b86ee23ae0cc7b2d50d0f1b9f
shaesen2 <haesendonckx.steven@gmail.com>
2020-06-30 18:24:15 +0200

    blah

--- R/add_CI.R
+++ R/add_CI.R
@@ -1,8 +1,8 @@
-add_KM_CI <- function(gg, ...){
-  UseMethod("add_KM_CI")
+add_CI <- function(gg, ...){
+  UseMethod("add_CI")
 } 
 
-add_KM_CI.ggKMsurv <- function(gg, ...){
+add_CI.ggKMsurv <- function(gg, ...){
 
   if (!"conf.int" %in% colnames(gg$data)){
     warning("Confidence limits were not part of original estimation.")
@@ -11,17 +11,11 @@ add_KM_CI.ggKMsurv <- function(gg, ...){
   
  ## CI associated with this:
     gg <- gg +
-<<<<<<< HEAD
       geom_ribbon(aes(ymin = est.lower, ymax = est.upper, fill = strata), alpha = 0.1) 
-=======
-      geom_ribbon(aes(ymin = lower, ymax = upper, fill = strata), alpha = 0.1) 
-    
-  } else if (y == "cumhaz"){
->>>>>>> 026ffff029aab733ae1d4fdd31b77b62f00ac333
     
   return(gg)
 }
 
-add_KM_CI.tblKM <- function(gg){
+add_CI.tblKM <- function(gg){
   stop("this object is not yet part of the scope")
 }
