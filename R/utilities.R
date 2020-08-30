a063ed8cbc9aeddfc3f029ef2e10ae5dca1ef68d
shaesen2 <haesendonckx.steven@gmail.com>
2020-08-30 16:25:11 +0200

    Documentation

--- R/utilities.R
+++ R/utilities.R
@@ -1,5 +1,26 @@
-### function that will find the "lhs" symbol in the pipeline, given that this function is called from with a pipeline
- ## https://github.com/tidyverse/magrittr/issues/115#issuecomment-173894787
+#' @title Find the "lhs" symbol in the pipeline
+#'  
+#' @description This function finds the left-hand sided symbol in a magrittr pipe.
+#'
+#' @author Steven Haesendonckx {shaesen2@@its.jnj.com}
+#' 
+#' @return Left-hand sided symbol as string in the magrittr pipe.
+#' 
+#' @references \url{https://github.com/tidyverse/magrittr/issues/115#issuecomment-173894787}
+#' 
+#' @export
+#'
+#' @examples
+#' library(magrittr)
+#' 
+#' ## load data
+#` load(file = file.path(getwd(), "data/adtte.rda"))
+#'  
+#' ## example
+#' blah <- function(x) the_lhs()
+#' adtte %>%
+#'   blah()
+
 the_lhs <- function() {
   parents <- lapply(sys.frames(), parent.env)
 
@@ -11,7 +32,37 @@ the_lhs <- function() {
   }
 }
 
-# https://stackoverflow.com/questions/26159495/align-multiple-ggplot-graphs-with-and-without-legends
+#' @title Align multiple ggplot graphs, taking into account the legend
+#'  
+#' @description This function aligns multiple ggplot graphs by making them the same width by taking into account the legend width.
+#'    
+#' @author Steven Haesendonckx {shaesen2@@its.jnj.com}
+#' 
+#' @return List of plots with equal width.
+#' 
+#' @references \url{https://stackoverflow.com/questions/26159495/align-multiple-ggplot-graphs-with-and-without-legends}
+#' 
+#' @examples
+#' library(ggplot2)
+#' library(gtable)
+#' library(cowplot)
+#' library(ggplot2)
+#' 
+#' ## load data
+#` load(file = file.path(getwd(), "data/adtte.rda"))
+#'  
+#' ## create 2 graphs
+#' p1 <- ggplot(adtte, aes(x = as.numeric(AGE), fill = "Age")) +
+#'   geom_histogram(bins = 15)
+#'   p2 <- ggplot(adtte, aes(x = as.numeric(AGE))) +
+#'     geom_histogram(bins = 15)
+#'     
+#' ## default alignment does not take into account legend size
+#' cowplot::plot_grid(plotlist = list(p1,p2), align = "none", nrow = length(plotlist))
+#' 
+#' ## Alignplots takes into account legend width
+#' cowplot::plot_grid(plotlist = AlignPlots(pltlist = list(p1, p2)), align = "none", nrow = length(plotlist))
+
 AlignPlots <- function(pltlist = NULL) {
   
   .LegendWidth <- function(x) x$grobs[[8]]$grobs[[1]]$widths[[4]]
@@ -37,7 +88,14 @@ AlignPlots <- function(pltlist = NULL) {
   plots.grobs.eq.widths.aligned
 }
 
-
+#' @title Experimental - Needs to take into account xlimits/ylimits
+#'     Translates options for legend into a list that can be passed to ggplot2
+#'  
+#' @description This function takes the legend position and orientation, defined by the user and puts them into a list for ggplot2.
+#'    
+#' @author Steven Haesendonckx {shaesen2@@its.jnj.com}
+#' 
+#' @return List of legend options for ggplot2.
 
 legendopts <- function(
   legend_position = "right",
