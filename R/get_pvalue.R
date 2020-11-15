1493a12fd4a838e80377607a853a1a700fa50a18
shaesen2 <haesendonckx.steven@gmail.com>
2020-11-15 10:57:27 +0100

    draft proposal for new set of tests

--- R/get_pvalue.R
+++ R/get_pvalue.R
@@ -11,10 +11,9 @@
 #' @param ... other arguments passed on to the method
 #' 
 #' @examples
+#' library(visR)
 #' library(survival)
-#' library(dplyr)
-#' library(tidyr)
-#' library(ggplot2)
+#' library(survMisc)
 #' 
 #' survfit_object <- vr_KM_est(data = adtte, strata = "TRTP")
 #' get_pvalue(survfit_object)
@@ -25,77 +24,112 @@
 #' 
 #' @export
 
+
+# T1. Calculate rho-family requested ptypes
+# T1.1 Logranks: specify ptype
+# T1.2 Wilcoxon: specify ptype
+# T1.3 Rho: 
+# T2. Calculate weighted log rank
+# 
+# T3. Calculate combinations of 
+# T3.1 Recognize valid ptypes
+# T3.2 Combinations but also independent
+
+
+
 get_pvalue <- function(x, ...){
   UseMethod("get_pvalue")
 }
 
 #' @param survfit_object An object of class \code{survfit}
-#' @param ptype Character vector containing the type of p-value desired. Current options are "Log-Rank" "Wilcoxon" "Tarone-Ware" "Custom" "All".
-#'    "Custom" allows the user to specify the weights on the Kaplan-Meier estimates using the argument `rho`.
-#'    The default is "All" displaying all types possible. When `rho` is specified in context of "All", also a custom p-value is displayed. 
+#' 
+#' @param ptype Character vector containing the type of test desired for calculation of the p-value. The difference in survival curves is calculated
+#'   based on weighted Log-Rank tests using \code{\link[survMisc]{comp}}. Options are:
+#'     "Log-Rank" "Gehan and Breslow" "Tharone-Ware" "Peto-Peto" "Andersen" "Fleming-Harrington".
+#'      The "Fleming-Harrington" method can be adjusted by specifying p and q.  \cr
+#'      The default is Log-Rank. A trend test can be done by including scores for the different strata.
+#'      
 #' @param statlist Character vector containing the desired information to be displayed. The order of the arguments determines the order in which
 #'    they are displayed in the final result. Default is the test name ("test"), Chisquare test statistic ("Chisq"), degrees of freedom ("df") and
 #'    p-value ("p").
-#' @inheritParams survival::survdiff
+#'    
+#' @inheritParams survMisc::comp
 #' 
-#'
 #' @rdname get_pvalue
 #' @method get_pvalue survfit
 #' @export
 
 get_pvalue.survfit <- function(survfit_object,
-                               ptype = "All",
-                               rho   = NULL,
-                               statlist = c("test", "Chisq", "df", "p"),
+                               ptype = c("Log-Rank"),
+                               statlist = c("Test", "Scores", "Weights", "p-value"),
+                               p = 1,
+                               q = 1,
+                               scores = NULL,
                                ...) {
-  ## Input Validation
   
+
+# Input Validation --------------------------------------------------------
+  
+  wt_test <- c("Log-Rank", "Gehan and Breslow", "Tharone-Ware", "Peto-Peto", "Andersen", "Fleming-Harrington")
+
   if (!inherits(survfit_object, "survfit"))
     stop("Error in get_pvalue: x is not of class `survfit`.")
+  
   if (length(names(survfit_object[["strata"]])) == 1)
-    stop(
-      "Error in get_pvalue: Main effect has only 1 level. Test of equality over strata can't be determined"
-    )
+    stop("Error in get_pvalue: Main effect has only 1 level. Test of equality over strata can't be determined")
+  
+  if (!is.null(scores) & length(names(survfit_object[["strata"]])) != length(scores))
+    stop("Error in get_pvalue: Test of Trend requires equal length of strata and scores.")
+  
+  if (length(names(survfit_object[["strata"]])) < 3 & !is.null(scores))
+    stop("Error in get_pvalue: Main effect has less than 3 levels. Test of Trend can't be performed.")
+  
   if (is.null(ptype))
     stop("Error in get_pvalue: Specify a valid ptype.")
-  if (!base::any(c("Log-Rank", "Wilcoxon", "Tarone-Ware", "Custom", "All") %in% ptype))
-    stop("Error in get_pvalue: Specify a valid type")
-  if ("Custom" %in% ptype & is.null(rho))
-    stop("Error in get_pvalue: ptype = `Custom`. Please, specify rho.")
-  if (is.null(statlist) |
-      !base::all(statlist %in% c("test", "df", "Chisq", "p")))
+  
+  if (!base::any(c(wt_test) %in% ptype))
+    stop("Error in get_pvalue: Specify a valid ptype")
+  
+  if (is.null(statlist) | !base::all(statlist %in% c("Test", "Scores", "Weights", "p-value")))
     stop("Error in get_pvalue: Specify valid `statlist` arguments.")
   
-  ## Re-use Call from survival object
+
+# Reset scores to default if NULL -----------------------------------------
+
+  if (is.null(scores)) scores = base::rep(1, length(survfit_object[["n"]]))
   
-  Call <- as.list(survfit_object$call)
-  NewCall <-
-    append(as.list(as.symbol("survdiff")), Call[names(Call) %in% names(formals(survival::survdiff))])
+  Scores <- rep(paste(scores, collapse = ", "), 6)
+
+# Summary List ------------------------------------------------------------
   
-  if ("All" %in% ptype) {
-    ptype = c("Log-Rank", "Wilcoxon", "Tarone-Ware")
-    if (!is.null(rho)) {
-      ptype = c(ptype, "Custom")
-    }
-  }
+  tenfit <- capture.output(comp(ten(survfit_object)))
+  splitters <- tenfit[grepl("\\$", tenfit)]
+  tenfit <- tenfit[which(grepl("\\$tft", tenfit)):which(grepl("\\$scores", tenfit))]
+  tenfit <- tenfit[-which(grepl("\\$", tenfit))]
+  tenfit <- tenfit[which(!grepl("Signif", tenfit))]
+  tenfit <- tenfit[tenfit != ""]
+  tenfit <- tenfit[which(!grepl("---", tenfit))]
+  tenfitlist <- strsplit(tenfit, "\\s+")
+  minlength <- min(unlist(lapply(tenfitlist, length)))
+  tenfitlist <- lapply(tenfitlist, function(x) x[1:minlength])
+  tenfitlist[[1]] <- NULL
+
+  tft <- as.data.frame(matrix(unlist(tenfitlist), nrow=length(tenfitlist), byrow=T), stringsAsFactors = FALSE)
+
   
-  ## Summary list
+  tft <- cbind(wt_test, Scores, tft)
   
-  survdifflist <- list(
-    `Log-Rank`    = rlang::expr(eval(as.call(
-      append(NewCall, list(rho = 0))
-    ))),
-    `Wilcoxon`    = rlang::expr(eval(as.call(
-      append(NewCall, list(rho = 1))
-    ))),
-    `Tarone-Ware` = rlang::expr(eval(as.call(
-      append(NewCall, list(rho = 1.5))
-    ))),
-    `Custom`      = rlang::expr(eval(as.call(
-      append(NewCall, list(rho = rho))
-    )))
-  )[ptype]
-  
+    
+  colnames(tft) <- c("Test", "Scores", "Weights", "Q", "var", "z", "p-value")
+
+
+# Statlist ----------------------------------------------------------------
+
+  statlist <- unique(statlist)
+
+# Selection ---------------------------------------------------------------
+
+  final <- tft[which(tft[,"Test"] %in% ptype), statlist]
   
   .pvalformat <- function(x) {
     options(scipen = 999)
@@ -110,50 +144,10 @@ get_pvalue.survfit <- function(survfit_object,
              width = 6)
   }
   
-  survdifflist_eval <-
-    lapply(survdifflist, eval, env = environment())
   
-  ## statlist
+# Formatted Results -----------------------------------------------------------------
   
-  statlist <- unique(statlist)
-  statlist <-
-    base::sub("test", "Equality across strata", statlist, fixed = TRUE)
-  statlist <- base::sub("p", "p-value", statlist, fixed = TRUE)
-  Nms <- names(survdifflist_eval)
-  
-  stat_summary <- list(
-    `Equality across strata` = rlang::expr(base::sub(
-      "Custom",
-      paste0("Harrington and Fleming test (rho = ", rho, ")"),
-      Nms,
-      fixed = TRUE
-    )),
-    `Chisq`    = rlang::expr(unlist(
-      lapply(survdifflist_eval, function(x)
-        x$chisq)
-    )),
-    df        = rlang::expr(unlist(
-      lapply(survdifflist_eval, function(x)
-        length(x$n) - 1)
-    )),
-    `p-value` = rlang::expr(unlist(
-      lapply(survdifflist_eval, function(x)
-        .pvalformat(
-          stats::pchisq(x$chisq, length(x$n) - 1, lower.tail = FALSE)
-        ))
-    ))
-  )[statlist]
-  
-  
-  ## output to tibble
-  
-  equality <- data.frame(
-    lapply(stat_summary, eval, env = environment()),
-    check.names = FALSE,
-    stringsAsFactors = FALSE,
-    row.names = NULL
-  ) %>%
-    tibble::as_tibble()
-  
-  return(equality)
+  if ("p-value" %in% statlist) final[["p-value"]] <- .pvalformat(final[["p-value"]])
+
+  return(final)
 } 
