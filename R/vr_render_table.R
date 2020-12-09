7ed063f310183afc6535ba24c120c6551eaadb0c
rebecca-albrecht <64608407+rebecca-albrecht@users.noreply.github.com>
2020-12-09 13:48:45 +0100

    Added a footnote to the three table options

--- R/vr_render_table.R
+++ R/vr_render_table.R
@@ -1,12 +1,13 @@
 #' Render a dataframe or tibble
 #'
-#' @description Render a previously created dataframe or tibble to html,
+#' @description Render a previously created dataframe to html,
 #' rtf or latex
 #'
 #'
 #' @param data The dataframe or tibble to visualise
 #' @param title Table title to include in the rendered table
 #' @param datasource String specifying the datasource underlying the data set
+#' @param footnote String specifying additional information to be displayed in the table note alongside the data source and specifications of statistical tests.
 #' @param output_format If TRUE, the summary statistics for the overall dataset
 #' are also calculated
 #' @param engine If html is selected as output format, one can chose between
@@ -18,6 +19,7 @@
 vr_render_table <- function(data,
                             title,
                             datasource,
+                            footnote = "",
                             output_format="html",
                             engine="gt",
                             download_format = c('copy', 'csv', 'excel')){
@@ -31,9 +33,11 @@ vr_render_table.vr_tableone <- function(
   data,
   title,
   datasource,
+  footnote = "",
   output_format="html",
   engine="gt",
   download_format = c('copy', 'csv', 'excel')){
+  
   sample <- data[data$variable == "Sample", ]
   sample <- sample[3:length(sample)]
   sample_names = colnames(sample)
@@ -43,99 +47,17 @@ vr_render_table.vr_tableone <- function(
   })
   colnames(data) <- c(colnames(data)[1:2], new_sample_names)
   data <- data[data$variable != "Sample", ]
-  vr_render_table.data.frame(data, title, datasource, output_format, engine, download_format)
+  vr_render_table.data.frame(data=data, title=title, datasource=datasource, footnote=footnote, output_format=output_format, engine=engine, download_format=download_format)
 }
 
 #' @rdname vr_render_table
-#' @method vr_render_table data.frame
-#' @export
-vr_render_table.data.frame <- function(
-  data,
-  title,
-  datasource,
-  output_format="html",
-  engine="gt",
-  download_format = c('copy', 'csv', 'excel')){
-  # TODO: add code for rtf output
-  # TODO: do we need a routine for falling back on minimal?
-  # TODO: do we need features to further specify styling of the table?
-  
-  check_rendering_input(output_format, engine)
-
-
-  #--------------------
-  # Kable output
-  if(tolower(engine) == "kable"){
-    if(tolower(output_format) %in% c("html", "latex")){
-      table_out <- data %>%
-        knitr::kable(format = output_format,
-                     caption = title,
-                     digits = 2,
-                     booktabs = T) %>%
-        kableExtra::collapse_rows(valign="top") %>%
-        kableExtra::footnote(general = datasource,
-                             general_title = "Data Source:")
-    }
-    else{
-      warning(paste("Supported output format of the kable engine are html and latex and not", output_format, " - falling back to html"))
-      vr_render_table(data=data, title=title, datasource=datasource,
-                         output_format="html", engine=engine, download_format=download_format)
-    }
-  }
-
-  #--------------------
-  # GT output
-  else if(tolower(engine) == "gt"){
-    if(!tolower(output_format) %in% c("html", "latex")){
-      warning(paste("Supported output format of the gt engine are html and latex and not", output_format, " - falling back to html"))
-    }
-
-    table_out <- render_gt(data, title, datasource)
-
-    if(output_format == "latex"){
-      # note: after this step, the table is not a gt object anymore and thus cannot be further styled
-      table_out <- table_out %>% gt::as_latex()
-    }
-  }
-
-  #--------------------
-  # jQuery DT output
-  else if(tolower(engine) %in% c("dt", "datatables", "datatable")){
-    if(!tolower(output_format) %in% c("html")){
-      warning(paste("DT engine only supports html output and not", output_format,
-                    "- falling back to html. Please pick a different engine to create other outputs"))
-    }
-    
-    # WIP: we may want to create a custom container to allow for stratification of more than one value and merge cells in the description
-    # sketch = htmltools::withTags(table(
-    #   DT::tableHeader(colnames(data)),
-    #   DT::tableFooter(paste("Source:", datasource))
-    # ))
-    
-    source_cap <- c(
-      "function(settings){",
-      "  var datatable = settings.oInstance.api();",
-      "  var table = datatable.table().node();",
-      paste("  var caption = 'Data Source:", datasource, "'"),
-      "  $(table).append('<caption style=\"caption-side: bottom\">' + caption + '</caption>');",
-      "}"
-    )
-
-    # may need some adjustment to also allow creation of DT in loops
-    table_out <- render_datatable(data, title, download_format, source_cap)
-  }
-
-  #--------------------
-  return(table_out)
-}
-
-#' @rdname vr_render_table
-#' @method vr_render_table default
+#' @method vr_render_table vr_risktable
 #' @export
 vr_render_table.vr_risktable <- function(
   data,
   title,
   datasource,
+  footnote = "",
   output_format="html",
   engine="gt",
   download_format = c('copy', 'csv', 'excel')){
@@ -164,6 +86,99 @@ vr_render_table.vr_risktable <- function(
                   engine,
                   download_format)
   
+}
+
+
+
+#' @rdname vr_render_table
+#' @method vr_render_table data.frame
+#' @export
+vr_render_table.data.frame <- function(
+  data,
+  title,
+  datasource,
+  footnote = "",
+  output_format="html",
+  engine="gt",
+  download_format = c('copy', 'csv', 'excel')){
+  # TODO: add code for rtf output
+  # TODO: do we need a routine for falling back on minimal?
+  # TODO: do we need features to further specify styling of the table?
+  
+  check_rendering_input(output_format, engine)
+
+
+  #--------------------
+  # Kable output
+  if(tolower(engine) == "kable"){
+    if(tolower(output_format) %in% c("html", "latex")){
+      table_out <- data %>%
+        knitr::kable(format = output_format,
+                     caption = title,
+                     digits = 2,
+                     booktabs = T) %>%
+        kableExtra::collapse_rows(valign="top") %>%
+        kableExtra::footnote(general = footnote, 
+                             general_title = "Additional Note:") %>%
+        kableExtra::footnote(general = datasource,
+                             general_title = "Data Source:") 
+        
+    }
+    else{
+      warning(paste("Supported output format of the kable engine are html and latex and not", output_format, " - falling back to html"))
+      vr_render_table(data=data, title=title, datasource=datasource,
+                         output_format="html", engine=engine, download_format=download_format)
+    }
+  }
+
+  #--------------------
+  # GT output
+  else if(tolower(engine) == "gt"){
+    if(!tolower(output_format) %in% c("html", "latex")){
+      warning(paste("Supported output format of the gt engine are html and latex and not", output_format, " - falling back to html"))
+    }
+
+    table_out <- render_gt(data=data, title=title, datasource=datasource, footnote=footnote)
+
+    if(output_format == "latex"){
+      # note: after this step, the table is not a gt object anymore and thus cannot be further styled
+      table_out <- table_out %>% gt::as_latex()
+    }
+  }
+
+  #--------------------
+  # jQuery DT output
+  else if(tolower(engine) %in% c("dt", "datatables", "datatable")){
+    if(!tolower(output_format) %in% c("html")){
+      warning(paste("DT engine only supports html output and not", output_format,
+                    "- falling back to html. Please pick a different engine to create other outputs"))
+    }
+    
+    # WIP: we may want to create a custom container to allow for stratification of more than one value and merge cells in the description
+    # sketch = htmltools::withTags(table(
+    #   DT::tableHeader(colnames(data)),
+    #   DT::tableFooter(paste("Source:", datasource))
+    # ))
+    footnote_1 <- paste0("Data Source: ", datasource)
+    caption_datasource <- paste("  var caption = 'Data Source:", datasource)
+    caption_compl <- paste(caption_datasource, footnote, "'", sep="; ")
+    source_cap <- c(
+      "function(settings){",
+      "  var datatable = settings.oInstance.api();",
+      "  var table = datatable.table().node();",
+      caption_compl,
+      #paste("  var caption = 'Data Source:", datasource, "'"),
+      "  $(table).append('<caption style=\"caption-side: bottom\">' + caption + '</caption>');",
+      #"  $(table).lastElementChild.replaceWith('<caption style=\"caption-side: bottom\">' + caption + '</caption>');",
+      "}"
+    )
+
+    # may need some adjustment to also allow creation of DT in loops
+    table_out <- render_datatable(data, title, download_format, source_cap)
+  }
+
+  #--------------------
+  return(table_out)
 }
 
 
@@ -217,7 +232,7 @@ render_datatable.data.frame <- function(data, title, download_format, source_cap
 
 
 ### Functions for gt rendering
-render_gt <- function(data, title, datasource){
+render_gt <- function(data, title, datasource, footnote){
   # identify numeric columns for special formatting later
   numcols <- data %>% dplyr::select_if(is.numeric) %>% names()
   # create gt table
@@ -226,7 +241,7 @@ render_gt <- function(data, title, datasource){
     gt::fmt_number(
       columns = numcols,
       decimals = 2
-    )%>% add_metadata_gt(title, datasource)
+    )%>% add_metadata_gt(title=title, datasource=datasource, footnote=footnote)
   return(table_out)
 }
 
@@ -250,12 +265,13 @@ create_gt.data.frame <- function(data, numcols){
 }
 
 # add metadata to gt
-add_metadata_gt <- function(gt, title, datasource){
+add_metadata_gt <- function(gt, title, datasource, footnote){
   table_out <- gt %>% gt::tab_header(title = title)
   
   table_out <- table_out %>%
     # add metadata
     gt::tab_source_note(source_note = paste("Data Source:", datasource)) %>%
+    gt::tab_source_note(source_note = footnote) %>%
     # add formatting
     gt::tab_options(data_row.padding = gt::px(4))
   
