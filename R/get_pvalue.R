8065e393c3a5c000cff257c2e3e1c19fdf65dc62
shaesen2 <shaesen2@its.jnj.com>
2021-02-24 12:58:09 -0500

    update from Alexandra

--- R/get_pvalue.R
+++ R/get_pvalue.R
@@ -1,28 +1,28 @@
 #' @title Summarize the test for equality across strata from a survival object using S3 method
 #'
 #' @description S3 method for extracting information regarding equality across strata.
-#'     No default method is available at the moment.
-#'     
+#' No default method is available at the moment.
+#'
 #' @author Steven Haesendonckx
-#' 
+#'
 #' @seealso \code{\link[survival]{survdiff}}
-#' 
+#'
 #' @param x S3 object
 #' @param ... other arguments passed on to the method
-#' 
+#'
 #' @examples
 #' library(survival)
 #' library(dplyr)
 #' library(tidyr)
 #' library(ggplot2)
-#' 
+#'
 #' survfit_object <- vr_KM_est(data = adtte, strata = "TRTP")
 #' get_pvalue(survfit_object)
-#'  
-#' @return A tibble with summary measures for the Test of Equality Across Strata
-#'  
+#'
+#' @return A dataframe with summary measures for the Test of Equality Across Strata
+#'
 #' @rdname get_pvalue
-#' 
+#'
 #' @export
 
 get_pvalue <- function(x, ...){
@@ -30,14 +30,17 @@ get_pvalue <- function(x, ...){
 }
 
 #' @param survfit_object An object of class \code{survfit}
-#' @param ptype Character vector containing the type of p-value desired. Current options are "Log-Rank" "Wilcoxon" "Tarone-Ware" "Custom" "All".
-#'    "Custom" allows the user to specify the weights on the Kaplan-Meier estimates using the argument `rho`.
-#'    The default is "All" displaying all types possible. When `rho` is specified in context of "All", also a custom p-value is displayed. 
+#' @param ptype Character vector containing the type of test for the p-value. Current options are "Log-Rank", "Wilcoxon", "Custom", "All".
+#'    "Custom" allows the user to specify the weights on the Kaplan-Meier estimates using the argument `rho`. When `rho` is specified in context of "All",
+#'    the G-rho family of Harrington and Fleming is displayed.
+#'    The default is "All" displaying all types possible.
+#'@param rho Scalar parameter which controls the type of test. `rho=0` is equivalent to the Mantel-Haenszel test (log-rank test) and `rho=1` to the
+#'    Peto and Peto modification of the Gehan-wilcoxon test.
 #' @param statlist Character vector containing the desired information to be displayed. The order of the arguments determines the order in which
 #'    they are displayed in the final result. Default is the test name ("test"), Chisquare test statistic ("Chisq"), degrees of freedom ("df") and
 #'    p-value ("p").
 #' @inheritParams survival::survdiff
-#' 
+#'
 #'
 #' @rdname get_pvalue
 #' @method get_pvalue survfit
@@ -49,7 +52,7 @@ get_pvalue.survfit <- function(survfit_object,
                                statlist = c("test", "Chisq", "df", "p"),
                                ...) {
   ## Input Validation
-  
+
   if (!inherits(survfit_object, "survfit"))
     stop("Error in get_pvalue: x is not of class `survfit`.")
   if (length(names(survfit_object[["strata"]])) == 1)
@@ -58,29 +61,29 @@ get_pvalue.survfit <- function(survfit_object,
     )
   if (is.null(ptype))
     stop("Error in get_pvalue: Specify a valid ptype.")
-  if (!base::any(c("Log-Rank", "Wilcoxon", "Tarone-Ware", "Custom", "All") %in% ptype))
+  if (!base::any(c("Log-Rank", "Wilcoxon",  "Custom", "All") %in% ptype))
     stop("Error in get_pvalue: Specify a valid type")
   if ("Custom" %in% ptype & is.null(rho))
     stop("Error in get_pvalue: ptype = `Custom`. Please, specify rho.")
   if (is.null(statlist) |
       !base::all(statlist %in% c("test", "df", "Chisq", "p")))
     stop("Error in get_pvalue: Specify valid `statlist` arguments.")
-  
+
   ## Re-use Call from survival object
-  
+
   Call <- as.list(survfit_object$call)
   NewCall <-
     append(as.list(as.symbol("survdiff")), Call[names(Call) %in% names(formals(survival::survdiff))])
-  
+
   if ("All" %in% ptype) {
-    ptype = c("Log-Rank", "Wilcoxon", "Tarone-Ware")
+    ptype = c("Log-Rank", "Wilcoxon")
     if (!is.null(rho)) {
       ptype = c(ptype, "Custom")
     }
   }
-  
+
   ## Summary list
-  
+
   survdifflist <- list(
     `Log-Rank`    = rlang::expr(eval(as.call(
       append(NewCall, list(rho = 0))
@@ -88,15 +91,12 @@ get_pvalue.survfit <- function(survfit_object,
     `Wilcoxon`    = rlang::expr(eval(as.call(
       append(NewCall, list(rho = 1))
     ))),
-    `Tarone-Ware` = rlang::expr(eval(as.call(
-      append(NewCall, list(rho = 1.5))
-    ))),
     `Custom`      = rlang::expr(eval(as.call(
       append(NewCall, list(rho = rho))
     )))
   )[ptype]
-  
-  
+
+
   .pvalformat <- function(x) {
     options(scipen = 999)
     if (x < 0.001)
@@ -109,18 +109,18 @@ get_pvalue.survfit <- function(survfit_object,
              justify = "right",
              width = 6)
   }
-  
+
   survdifflist_eval <-
     lapply(survdifflist, eval, env = environment())
-  
+
   ## statlist
-  
+
   statlist <- unique(statlist)
   statlist <-
     base::sub("test", "Equality across strata", statlist, fixed = TRUE)
   statlist <- base::sub("p", "p-value", statlist, fixed = TRUE)
   Nms <- names(survdifflist_eval)
-  
+
   stat_summary <- list(
     `Equality across strata` = rlang::expr(base::sub(
       "Custom",
@@ -130,7 +130,7 @@ get_pvalue.survfit <- function(survfit_object,
     )),
     `Chisq`    = rlang::expr(unlist(
       lapply(survdifflist_eval, function(x)
-        x$chisq)
+        round(x$chisq,2)) # put round
     )),
     df        = rlang::expr(unlist(
       lapply(survdifflist_eval, function(x)
@@ -143,17 +143,16 @@ get_pvalue.survfit <- function(survfit_object,
         ))
     ))
   )[statlist]
-  
-  
+
+
   ## output to tibble
-  
+
   equality <- data.frame(
     lapply(stat_summary, eval, env = environment()),
     check.names = FALSE,
     stringsAsFactors = FALSE,
     row.names = NULL
-  ) %>%
-    tibble::as_tibble()
-  
+  )
+
   return(equality)
-} 
+}
