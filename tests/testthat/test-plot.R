248f219088acb7f9d4b540e3b1f663ae54738f61
Tim Treis <tim.treis@outlook.de>
2021-03-04 09:26:19 +0100

    Added test for endpoints of plot

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -10,15 +10,19 @@ library(testthat)
 library(visR)
 library(survival)
 
-#' T1. The function accepts a `survfit` object and passes other objects to base
+#' T1.  The function accepts a `survfit` object and passes other objects to base
 #' T1.1 No error when `survfit_object` is of class `survfit`
 #' T1.2 An error when `survfit_object` is not of class `survfit`
 #' T1.3 No error when a non-`survfit` object is passed to it
 #' T1.4 An error when `survfit_object` is NULL
 #' T1.5 An error when `survfit_object` does not exist in the global environment
+#' T2.  The function produces a survival ggplot2 survival plot 
+#' T2.1 The class vector of the generated output contains `gg`
+#' T2.2 The class vector of the generated output contains `ggplot`
+#' T2.3 The class vector of the generated output contains `ggsurvfit`
 
 
-# Requirement T1 ----------------------------------------------------------
+# Requirement T1 ---------------------------------------------------------------
 
 context("plot - T1. The function accepts a `survfit` object and passes other objects to base")
 
@@ -75,9 +79,53 @@ testthat::test_that("T1.5 An error when `survfit_object` does not exist in the g
 
 
 
-# Requirement T2 ----------------------------------------------------------
+# Requirement T2 ---------------------------------------------------------------
 
+context("plot - T2. The function produces a survival ggplot2 survival plot")
 
+testthat::test_that("T2.1 The class vector of the generated output contains `gg`",{
+  
+  survfit_object <- adtte %>% visR::estimate_KM(strata = "TRTP")
+  p <- visR::plot(survfit_object = survfit_object)
+  
+  testthat::expect_true("gg" %in% class(p))
+  
+})
+
+testthat::test_that("T2.2 The class vector of the generated output contains `ggplot`",{
+  
+  survfit_object <- adtte %>% visR::estimate_KM(strata = "TRTP")
+  p <- visR::plot(survfit_object = survfit_object)
+  
+  testthat::expect_true("ggplot" %in% class(p))
+  
+})
+
+testthat::test_that("T2.3 The class vector of the generated output contains `ggsurvfit`",{
+  
+  survfit_object <- adtte %>% visR::estimate_KM(strata = "TRTP")
+  p <- visR::plot(survfit_object = survfit_object)
+  
+  testthat::expect_true("ggsurvfit" %in% class(p))
+  
+})
+
+testthat::test_that("T2.4 The resulting plot contains the correct amount of strata with correct endpoints",{
+  
+  survfit_object <- adtte %>% visR::estimate_KM(strata = "SEX")
+  p <- visR::plot(survfit_object = survfit_object)
+  p_data <- ggplot2::ggplot_build(p)$data[[1]]
+  p_layout <- ggplot2::ggplot_build(p)$layout
+  
+  n_sex <- length(unique(p_data$colour))
+  endpoint_group1 <- min(p_data[p_data$group == "1",]$y)
+  endpoint_group2 <- min(p_data[p_data$group == "2",]$y)
+  
+  testthat::expect_equal(n_sex, 2)
+  testthat::expect_equal(round(endpoint_group1, 2), 0.34)
+  testthat::expect_equal(round(endpoint_group2, 2), 0.26)
+  
+})
 
 # END OF CODE ----------------------------------------------------------
 
