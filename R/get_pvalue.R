c5f6e76fcd4b6c94e9c548e07721d7e1949c3e74
shaesen2 <shaesen2@its.jnj.com>
2021-03-19 05:43:48 -0400

    initial setup

--- R/get_pvalue.R
+++ R/get_pvalue.R
@@ -2,40 +2,36 @@
 #'
 #' @description S3 method for extracting information regarding equality across strata.
 #'     No default method is available at the moment.
-#'     
+#'
 #' @author Steven Haesendonckx
-#' 
+#'
 #' @seealso \code{\link[survival]{survdiff}}
-#' 
+#'
 #' @param x S3 object
 #' @param ... other arguments passed on to the method
-#' 
+#'
 #' @examples
-#' library(survival)
-#' library(dplyr)
-#' library(tidyr)
-#' library(ggplot2)
-#' 
-#' survfit_object <- estimate_KM(data = adtte, strata = "TRTP")
-#' get_pvalue(survfit_object)
-#' get_pvalue(survfit_object, ptype = "All")
-#' 
+#'
+#' survfit_object <- visR::estimate_KM(data = adtte, strata = "TRTP")
+#' visR::get_pvalue(survfit_object)
+#' visR::get_pvalue(survfit_object, ptype = "All")
+#'
 #' ## examples to obtain specific tests
-#' get_pvalue(survfit_object, ptype = "Log-Rank")
-#' get_pvalue(survfit_object, ptype = "Wilcoxon")
-#' get_pvalue(survfit_object, ptype = "Tarone-Ware")
-#' 
+#' visR::get_pvalue(survfit_object, ptype = "Log-Rank")
+#' visR::get_pvalue(survfit_object, ptype = "Wilcoxon")
+#' visR::get_pvalue(survfit_object, ptype = "Tarone-Ware")
+#'
 #' # Custom example - obtain Harrington and Fleming test
-#' get_pvalue(survfit_object, ptype = "Custom", rho = 1)
-#'  
-#' # Get specific information and statistics 
-#' get_pvalue(survfit_object, ptype = "Log-Rank", statlist = c("test", "Chisq", "df", "p"))
-#' get_pvalue(survfit_object, ptype = "Wilcoxon", statlist = c("p"))
+#' visR::get_pvalue(survfit_object, ptype = "Custom", rho = 1)
+#'
+#' # Get specific information and statistics
+#' visR::get_pvalue(survfit_object, ptype = "Log-Rank", statlist = c("test", "Chisq", "df", "p"))
+#' visR::get_pvalue(survfit_object, ptype = "Wilcoxon", statlist = c("p"))
 #'
 #' @return A tibble with summary measures for the Test of Equality Across Strata
-#'  
+#'
 #' @rdname get_pvalue
-#' 
+#'
 #' @export
 
 get_pvalue <- function(x, ...){
@@ -45,12 +41,12 @@ get_pvalue <- function(x, ...){
 #' @param survfit_object An object of class \code{survfit}
 #' @param ptype Character vector containing the type of p-value desired. Current options are "Log-Rank" "Wilcoxon" "Tarone-Ware" "Custom" "All".
 #'    "Custom" allows the user to specify the weights on the Kaplan-Meier estimates using the argument `rho`.
-#'    The default is "All" displaying all types possible. When `rho` is specified in context of "All", also a custom p-value is displayed. 
+#'    The default is "All" displaying all types possible. When `rho` is specified in context of "All", also a custom p-value is displayed.
 #' @param statlist Character vector containing the desired information to be displayed. The order of the arguments determines the order in which
 #'    they are displayed in the final result. Default is the test name ("test"), Chisquare test statistic ("Chisq"), degrees of freedom ("df") and
 #'    p-value ("p").
 #' @inheritParams survival::survdiff
-#' 
+#'
 #'
 #' @rdname get_pvalue
 #' @method get_pvalue survfit
@@ -61,14 +57,11 @@ get_pvalue.survfit <- function(survfit_object,
                                rho   = NULL,
                                statlist = c("test", "Chisq", "df", "p"),
                                ...) {
-  ## Input Validation
-  
-  if (!inherits(survfit_object, "survfit"))
-    stop("Error in get_pvalue: x is not of class `survfit`.")
-  if (length(names(survfit_object[["strata"]])) == 1)
-    stop(
-      "Error in get_pvalue: Main effect has only 1 level. Test of equality over strata can't be determined"
-    )
+
+# Input validation --------------------------------------------------------
+
+  if (length(names(survfit_object[["strata"]])) <= 1)
+    stop("Error in get_pvalue: Main effect has only 1 level. Test of equality over strata can't be determined.")
   if (is.null(ptype))
     stop("Error in get_pvalue: Specify a valid ptype.")
   if (!base::any(c("Log-Rank", "Wilcoxon", "Tarone-Ware", "Custom", "All") %in% ptype))
@@ -78,20 +71,20 @@ get_pvalue.survfit <- function(survfit_object,
   if (is.null(statlist) |
       !base::all(statlist %in% c("test", "df", "Chisq", "p")))
     stop("Error in get_pvalue: Specify valid `statlist` arguments.")
-  
+
   ## Re-use Call from survival object
-  
+
   Call <- as.list(survfit_object$call)
   NewCall <-
     append(as.list(parse(text = "survival::survdiff")), Call[names(Call) %in% names(formals(survival::survdiff))])
-  
+
   if ("All" %in% ptype) {
     ptype = c("Log-Rank", "Wilcoxon", "Tarone-Ware")
     if (!is.null(rho)) {
       ptype = c(ptype, "Custom")
     }
   }
-  
+
   ## Summary list
   survdifflist <- list(
     `Log-Rank`    = rlang::expr(eval(as.call(
@@ -107,8 +100,8 @@ get_pvalue.survfit <- function(survfit_object,
       append(NewCall, list(rho = rho))
     )))
   )[ptype]
-  
-  
+
+
   .pvalformat <- function(x) {
     options(scipen = 999)
     if (x < 0.001)
@@ -121,18 +114,18 @@ get_pvalue.survfit <- function(survfit_object,
              justify = "right",
              width = 6)
   }
-  
+
   survdifflist_eval <-
     lapply(survdifflist, eval, env = environment())
-  
+
   ## statlist
-  
+
   statlist <- unique(statlist)
   statlist <-
     base::sub("test", "Equality across strata", statlist, fixed = TRUE)
   statlist <- base::sub("p", "p-value", statlist, fixed = TRUE)
   Nms <- names(survdifflist_eval)
-  
+
   stat_summary <- list(
     `Equality across strata` = rlang::expr(base::sub(
       "Custom",
@@ -155,10 +148,10 @@ get_pvalue.survfit <- function(survfit_object,
         ))
     ))
   )[statlist]
-  
-  
+
+
   ## output to tibble
-  
+
   equality <- data.frame(
     lapply(stat_summary, eval, env = environment()),
     check.names = FALSE,
@@ -166,6 +159,6 @@ get_pvalue.survfit <- function(survfit_object,
     row.names = NULL
   ) %>%
     tibble::as_tibble()
-  
+
   return(equality)
-} 
+}
--- tests/testthat/test-get_pvalue.R
+++ tests/testthat/test-get_pvalue.R
@@ -1,9 +1,69 @@
-#' T1. The function accepts a survival object
-#' T1.1 No error when `survfit_object` is a survfit object
-#' T1.2 An error when `survfit_object` is a data.frame
-#' T1.3 An error when `survfit_object` is a tibble
-#' T1.4 An error when `survfit_object` is a data.table
-#' T1.5 An error when `survfit_object` is a random object
-#' T1.6 An error when `survfit_object` is NULL
+#' @title Specifications get_pvalue
+#' @section Last updated by:
+#' Steven Haesendonckx
+#' @section Last update date:
+#' 19-MAR-2021
 
-# Did not continue because Alexandra already started?
+# Specifications ----------------------------------------------------------
+
+#' T1. The function accepts a `survfit` object with at least 2 strata
+#' T1.1 No error when a `survfit` object is passed to the function with at least 2 strata
+#' T1.2 An error when a `survfit` object is passed to the function with 1 strata
+#' T1.3 An error when a non-`survfit` object is passed to the function
+#'
+#'
+#' T2.1 An error when colname specified through `AVAL` is not present in `data`
+#' T2.2 An error when colname specified through `AVAL` is not numeric
+#' T2.3 No error when the colname specified through `AVAL` is not the proposed default
+#' T2.4 An error when colname specified through `CNSR` is not present in `data`
+#' T2.5 An error when colname specified through `CNSR` is not numeric
+#' T2.6 No error when the colname specified through `CNSR` is not the proposed default
+#' T3. The user can specify strata
+#' T3.1 An error when the columns, specifying the strata are not available in `data`
+#' T3.2 No error when strata is NULL
+#' T3.3 When no strata are specified, an artificial strata is created 'Overall'
+#' T4. The function removes all rows with NA values inside any of the strata
+#' T5. The function does not alter the calculation of survival::survfit
+#' T5.1 The function gives the same results as survival::survfit
+#' T5.2 The function adds timepoint = 0
+#' T5.3 The function allows additional arguments to be passed, specific for survival::survfit
+#' T5.4 The function returns an object of class `survfit`
+#' T6. The function adds additional information to the survfit object when available
+#' T6.1 The calculation is not affected by the addition of additional parameters
+#' T6.2 The function add PARAM/PARAMCD when available
+#' T7. The function call supports traceability
+#' T7.1 The function updates call$data when magrittr pipe is used
+
+# Requirement T1 ----------------------------------------------------------
+
+context("get_pvalue - T1. The function accepts a `survfit` object")
+
+testthat::test_that("T1.1. No error when a `survfit` object is passed to the function with at least 2 strata",{
+
+  survfit_object <- visR::estimate_KM(adtte, strata = "TRTA")
+  testthat::expect_error(visR::get_pvalue(survfit_object), NA)
+
+})
+
+testthat::test_that("TT1.2 An error when a `survfit` object is passed to the function with 1 strata",{
+
+  survfit_object <- visR::estimate_KM(adtte, strata = "STUDYID")
+  testthat::expect_error(visR::get_pvalue(survfit_object))
+
+  names(survfit_object$strata)
+
+})
+
+testthat::test_that("TT1.3 An error when a non-`survfit` object is passed to the function",{
+
+  survfit_object <- visR::estimate_KM(adtte, strata = "TRTA")
+  class(survfit_object) <- "blah"
+  testthat::expect_error(visR::get_pvalue(survfit_object))
+
+})
+
+
+
+
+# END OF CODE ----------------------------------------------------------
+
