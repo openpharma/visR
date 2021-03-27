dbaf9ca3627ddc5128342de57d83b934c21b97f9
Tim Treis <tim.treis@outlook.de>
2021-03-28 00:00:17 +0100

    Wrote better tests.

--- tests/testthat/test-tidyme.R
+++ tests/testthat/test-tidyme.R
@@ -8,80 +8,121 @@
 
 #' T1.  The function tidies up an associated object
 #' T1.1 The default method returns a tibble, tidies with broom
-#' T1.2 The default method does not affect the content of the object eg `visR::tidyme(stats::lm())`
-#' T1.3 The default method throws a warning and relies on broom::tidy
-#' T2 The S3 method, associated with a survival object, returns a tidied survival object
+#' T1.2 The default method does not affect the content of the object, f.e. `visR::tidyme(stats::lm())`
+#' T1.3 The default method throws a message and relies on broom::tidy
+#' T2   The S3 method, associated with a survival object, returns a tidied survival object
 #' T2.1 The S3 method, associated with a survival object, transforms the survival object into a data frame
 #' T2.2 The tidied dataframe has the same content as the original survival object
-#' T2.3 The tidied dataframe contains the call of the original function
-#' T2.4 The tidied dataframe contains ... what else??
 
 # Requirement T1 ---------------------------------------------------------------
 
-context("tidyme - The function properly converts \"messy output\" into tidy tibbles")
+context("tidyme - The function tidies up an associated object")
 
-testthat::test_that("T1.1 The `visR::tidyme(stats::lm())` transformation doesn't change the values",{
+testthat::test_that("T1.1 The default method returns a tibble, tidies with broom",{
   
   lm_object <- stats::lm(data = adtte, TRTDUR ~ AVAL)
-  intercept <- lm_object$coefficients[[1]]
-  slope <- lm_object$coefficients[[2]]
+  testthat::expect_s3_class(lm_object, "lm")
   
   lm_tidied <- suppressMessages(visR::tidyme(lm_object))
-  tidy_intercept <- lm_tidied[lm_tidied$term == "(Intercept)", "estimate"][[1]]
-  tidy_slope <- lm_tidied[lm_tidied$term == "AVAL", "estimate"][[1]]
+  testthat::expect_s3_class(lm_tidied, "tbl")
   
-  testthat::expect_equal(79.62, round(intercept, 2))
-  testthat::expect_equal(round(intercept, 2), round(tidy_intercept, 2))
-  
-  testthat::expect_equal(0.55, round(slope, 2))
-  testthat::expect_equal(round(slope, 2), round(tidy_slope, 2))
+  lm_broomed <- broom::tidy(lm_object)
+  testthat::expect_identical(lm_tidied, lm_broomed)
   
 })
 
-testthat::test_that("T1.1 The `visR::tidyme(survival::survfit())` transformation doesn't change the values",{
-  
-  surv_object <- survival::survfit(data = adtte, Surv(AVAL, 1-CNSR) ~ TRTP)
-  surv <- surv_object$surv[c(5, 10, 50, 100)]
-  cumhaz <- surv_object$cumhaz[c(5, 10, 50, 100)]
-  
-  surv_object_tidied <- visR::tidyme(surv_object)
-  surv_tidied <- surv_object_tidied$surv[c(5, 10, 50, 100)]
-  cumhaz_tidied <- surv_object_tidied$cumhaz[c(5, 10, 50, 100)]
-  
-  testthat::expect_equal(c(0.94, 0.92, 0.63, 0.11), round(surv, 2))
-  testthat::expect_equal(round(surv, 2), round(surv_tidied, 2))
-  
-  testthat::expect_equal(c(0.06, 0.09, 0.46, 2.07), round(cumhaz, 2))
-  testthat::expect_equal(round(cumhaz, 2), round(cumhaz_tidied, 2))
-  
-})
-
-
-# Requirement T2 ---------------------------------------------------------------
-
-context("tidyme - T2. Warnings and errors are thrown when neccecary")
-
-testthat::test_that("T2.1 No error when a `stats::lm()` model is transformed",{
+testthat::test_that("T1.2 The default method does not affect the content of the object, f.e. `visR::tidyme(stats::lm())`",{
   
   lm_object <- stats::lm(data = adtte, TRTDUR ~ AVAL)
+  lm_tidied <- suppressMessages(visR::tidyme(lm_object))
   
-  testthat::expect_error(visR::tidyme(lm_object), NA)
+  lm_object_values <- coefficients(summary(lm_object)) %>% as.matrix()
+  
+  stats_colnames <- c("Estimate", "Std. Error", "t value", "Pr(>|t|)")
+  broom_colnames  <- c("estimate", "std.error", "statistic",  "p.value")
+  
+  for (i in 1:length(stats_colnames)) {
+    
+    for (j in 1:2) {
+      
+      testthat::expect_equal(lm_object_values[j, stats_colnames[i]],
+                             lm_tidied[j, broom_colnames[i]][[1]]) 
+      
+    }
+    
+  }
   
 })
 
-testthat::test_that("T2.2 No error when a `survival::survfit()` model is transformed",{
-  
-  surv_object <- survival::survfit(data = adtte, Surv(AVAL, 1-CNSR) ~ TRTP)
-  
-  testthat::expect_error(visR::tidyme(surv_object), NA)
-  
-})
-
-testthat::test_that("T2.3 Message when a non-survfit object is transformed",{
+testthat::test_that("T1.3 The default method throws a message and relies on broom::tidy",{
   
   lm_object <- stats::lm(data = adtte, TRTDUR ~ AVAL)
   
   testthat::expect_message(visR::tidyme(lm_object))
+  
+})
+
+# Requirement T2 ---------------------------------------------------------------
+
+context("tidyme - The S3 method, associated with a survival object, returns a tidied survival object")
+
+testthat::test_that("T2.1 The S3 method, associated with a survival object, transforms the survival object into a data frame",{
+  
+  surv_object <- survival::survfit(data = adtte, Surv(AVAL, 1-CNSR) ~ TRTP)
+  testthat::expect_s3_class(surv_object, "survfit")
+  
+  surv_object_tidied <- visR::tidyme(surv_object)
+  testthat::expect_s3_class(surv_object_tidied, "data.frame")
+  
+})
+
+testthat::test_that("T2.2 The tidied dataframe has the same content as the original survival object",{
+  
+  surv_object <- survival::survfit(data = adtte, Surv(AVAL, 1-CNSR) ~ TRTP)
+  
+  # Shape fit into df
+  surv_object_df <- base::with(surv_object, data.frame(time, 
+                                                       n.risk,
+                                                       n.event,
+                                                       n.censor,
+                                                       surv,
+                                                       std.err,
+                                                       cumhaz,
+                                                       std.chaz,
+                                                       type,
+                                                       logse,
+                                                       conf.int,
+                                                       conf.type,
+                                                       lower,
+                                                       upper))
+  
+  surv_object_df["call"] <- capture.output(surv_object$call)
+  surv_object_df["strata"] <- base::summary(surv_object, censored = T)$strata
+  n_per_strata_table <- base::summary(surv_object, censored = T)$table %>%
+    data.frame()
+  n_per_strata <- n_per_strata_table$records
+  n_per_strata <- setNames(as.list(n_per_strata), rownames(n_per_strata_table))
+  
+  n_strata_list <- c()
+  
+  for (strata in surv_object_df$strata) {
+    
+    n_strata_list <- c(n_strata_list, n_per_strata[[strata]])
+    
+  }
+  
+  surv_object_df["n.strata"] <- n_strata_list
+  
+  surv_object_df <- surv_object_df[with(surv_object_df, order(surv)),] 
+
+  surv_object_tidied <- visR::tidyme(surv_object)
+  
+  surv_object_tidied$call <- base::as.character(surv_object_tidied$call)
+  surv_object_tidied <- surv_object_tidied %>% as.data.frame() 
+  surv_object_tidied <- surv_object_tidied[with(surv_object_tidied, order(surv)),] 
+  surv_object_tidied["strata"] <- as.factor(surv_object_tidied$strata)
+  
+  testthat::expect_equal(surv_object_df, surv_object_tidied)
   
 })
 
