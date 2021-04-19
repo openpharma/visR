121a9a052f145d4c8b1e4f2ac6c5a87f2e8ceb44
shaesen2 <shaesen2@its.jnj.com>
2021-04-19 10:14:53 -0400

    Added testing - start

--- tests/testthat/test-get_COX_HR.R
+++ tests/testthat/test-get_COX_HR.R
@@ -1,7 +1,46 @@
-#' T1. The function accepts a survival object
-#' T1.1 No error when `survfit_object` is a survfit object
-#' T1.2 An error when `survfit_object` is a data.frame
-#' T1.3 An error when `survfit_object` is a tibble
-#' T1.4 An error when `survfit_object` is a data.table
-#' T1.5 An error when `survfit_object` is a random object
-#' T1.6 An error when `survfit_object` is NULL
+#' @title Specifications get_COX_HR
+#' @section Last updated by:
+#' Steven Haesendonckx
+#' @section Last update date:
+#' 19-APR-2021
+
+# Specifications ----------------------------------------------------------
+
+#' T1. The function accepts a `survfit` object
+#' T1.1 No error when the input is a `survfit` object
+#' T1.2 An error when the input is a non-`survfit` object
+#' T1.3 An error when the input is NULL
+#' T2. The function accepts an update to the original formula of the `survfit` object
+#' T2.1 No error when the update_formula argument is a `formula`
+#' T2.2 An error when the update_formula argument is not a `formula`
+#' T3. The function calculate the COX Hazard Ratio
+#' 
+#' 
+
+
+# Requirement T1 ----------------------------------------------------------
+
+context("get_COX_HR - T1. The function accepts a `survfit` object")
+
+testthat::test_that("T1.1 No error when the input is a `survfit` object",{
+
+  survfit_object <- visR::estimate_KM(adtte, strata = "TRTA")
+  testthat::expect_error(visR::get_COX_HR(survfit_object), NA)
+
+})
+
+testthat::test_that("T1.2 An error when the input is a non-`survfit` object",{
+
+  survfit_object <- as.list("blah")
+  testthat::expect_error(visR::get_COX_HR(survfit_object))
+
+})
+
+testthat::test_that("T1.3 An error when the input is NULL",{
+
+  survfit_object <- NULL
+  testthat::expect_error(visR::get_COX_HR(survfit_object))
+})
+
+# END OF CODE ----------------------------------------------------------
+
