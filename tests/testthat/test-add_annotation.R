de02b6634be62746d2c18cc9876a63fbef87ff4e
shaesen2 <haesendonckx.steven@gmail.com>
2021-05-02 09:26:24 +0200

    update

--- tests/testthat/test-add_annotation.R
+++ tests/testthat/test-add_annotation.R
@@ -7,10 +7,12 @@
 # Specifications ----------------------------------------------------------
 
 #' T1. The function adds annotations to an object of class `ggplot`
-#' ...
+#' T1.1 No error when a `ggplot` object is passed to the function in the presence of a label
+#' T1.2 An error when a non-`ggplot` object is passed to the function in the presence of a label
+#' T1.3 An error when NULL is passed to the function in the presence of a label
 #' T2. The function accepts a label of class `character`, `data.frame` or customized objects of class `gtable`
-#' T2.1 No error when the label specified is of class `character`
-#' ... NULL ...
+#' T2.1 An error when a `ggplot` object is passed to the function in the absence of a label
+#' T2.2 No error when label is of class `character`
 #' 
 #' T3. The annotation are representations of the actual label
 #' T3.1 The actual object passed to label is not affected by the transformation to an annotation
@@ -38,14 +40,63 @@
 
 context("add_annotation - T1. The function adds annotations to an object of class `ggplot`")
 
-testthat::test_that("T1.1 No error when a `ggplot` object is passed to the function",{
+testthat::test_that("T1.1 No error when a `ggplot` object is passed to the function in the presence of a label",{
 
-  # visR_plot <- visR::estimate_KM(data = adtte, strata = "TRTA") %>% visR::plot()
-  # 
-  # testthat::expect_error(visR::add_risktable(visR_plot), NA)
+  visR_plot <- visR::estimate_KM(data = adtte, strata = "TRTA") %>% visR::plot()
+
+  testthat::expect_error(visR::add_annotation(visR_plot, label = "blah"), NA)
 
 })
 
+testthat::test_that("T1.2 An error when a non-`ggplot` object is passed to the function in the presence of a label",{
+  
+  visR_plot <- "blah"
+
+  testthat::expect_error(visR::add_annotation(gg = visR_plot, label = "blah"))
+
+})
+
+testthat::test_that("T1.3 An error when NULL is passed to the function in the presence of a label",{
+
+  testthat::expect_error(visR::add_annotation(gg = NULL, label = "blah"))
+
+})
+
+
+# Requirement T2 ----------------------------------------------------------
+
+context("add_annotation - T2. The function accepts a label of class `character`, `data.frame` or customized objects of class `gtable`")
+
+testthat::test_that("T2.1 An error when a `ggplot` object is passed to the function in the absence of a label",{
+
+  visR_plot <- visR::estimate_KM(data = adtte, strata = "TRTA") %>% visR::plot()
+
+  testthat::expect_error(visR::add_annotation(visR_plot, label = NULL), NA)
+
+})
+
+testthat::test_that("T2.2 No error when label is of class `character`",{
+
+  visR_plot <- visR::estimate_KM(data = adtte, strata = "TRTA") %>% visR::plot()
+
+  testthat::expect_error(visR::add_annotation(visR_plot, label = "blah"), NA)
+
+})
+
+testthat::test_that("T2.3 No error when label is of class `data.frame`",{
+
+  visR_plot <- visR::estimate_KM(data = adtte, strata = "TRTA") %>% visR::plot()
+
+  testthat::expect_error(visR::add_annotation(visR_plot, label = adtte[1:5,]), NA)
+
+})
+
+
+
+#' T2. 
+#' T2.1 No error when the label specified is of class `character`
+#' ... NULL ...
+#' 
 
 # END OF CODE ----------------------------------------------------------
 
