6764bdc882ec238ff40d6de508168d509d6d2fea
rebecca-albrecht <64608407+rebecca-albrecht@users.noreply.github.com>
2021-06-02 09:11:04 +0200

    Fixed a bug in the documentation of the visr function and rebuilt said documentation.

--- R/visr.R
+++ R/visr.R
@@ -52,13 +52,13 @@ visr.default <- function(x, ...){
 #' @param fun Change the scale of the estimate. 
 #'   \itemize{
 #'   The current options are:
-#'   \item{`surv` is the survival probability. This is the default.}
-#'   \item{`log` is log of the survival probability}
-#'   \item{`event` is the failure probability}
-#'   \item{`cloglog` is log(-log(survival probability))}
-#'   \item{`pct` is survival as a percentage}
-#'   \item{`logpct` is log survival as a percentage}
-#'   \item{`cumhaz` is the cumulative hazard}
+#'   \item{`surv`} {is the survival probability. This is the default.}
+#'   \item{`log`} {is log of the survival probability}
+#'   \item{`event`} {is the failure probability}
+#'   \item{`cloglog`} {is log(-log(survival probability))}
+#'   \item{`pct`} {is survival as a percentage}
+#'   \item{`logpct`} {is log survival as a percentage}
+#'   \item{`cumhaz`} {is the cumulative hazard}
 #'   } 
 #' @param legend_position Specifies the legend position in the plot. 
 #'   Character values allowed are "top" "left" "bottom" "right". 
