5ae785108072dfde38a83b4ba5b70a7301fab609
Tim Treis <tim.treis@outlook.de>
2021-06-10 16:58:00 +0200

    Added code to restore user's default options after changes

--- R/utils_general.R
+++ R/utils_general.R
@@ -2,7 +2,11 @@
 #' @param x Input p-value. Numeric value. 
 #' @noRd
 .pvalformat <- function(x) {
+  
+  old <- options()
+  on.exit(options(old)) 
   options(scipen = 999)
+  
   if (x < 0.001)
     "<0.001"
   else if (x > 0.999)
