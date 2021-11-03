ab97982c295b72b049d1780cb3f189fe67c70988
Tim Treis <tim.treis@outlook.de>
2021-11-03 14:27:31 +0100

    added more sanity checks

--- R/add_quantiles.R
+++ R/add_quantiles.R
@@ -1,4 +1,4 @@
-#' @title Add lines indicating the survival quantiles to a survival plot
+#' @title Add quantile indicators to visR plot
 #'
 #' @description Method to add lines indicating the specified quantiles of the survival curve.
 #'
@@ -35,10 +35,10 @@ add_quantiles <- function(gg, ...){
 }
 
 #' @param gg A ggplot created with visR
-#' @param quantiles y-axis values for which to draw the lines (default: 0.5)
-#' @param linetype aesthetic of ggplot2 \code{\link[ggplot2]{geom_line}} (default: dashed)
-#' @param linecolour aesthetic of ggplot2 \code{\link[ggplot2]{geom_line}} (default: black)
-#' @param alpha aesthetic of ggplot2 \code{\link[ggplot2]{geom_line}} (default: 0.8)
+#' @param quantiles vector of quantiles to be displayed on the probability scale, default: 0.5
+#' @param linetype string indicating the linetype as described in the aesthetics of ggplot2 \code{\link[ggplot2]{geom_line}}, default: mixed (horizontal lines are solid, vertical ones are dashed)
+#' @param linecolour string indicating the linetype as described in the aesthetics of ggplot2 \code{\link[ggplot2]{geom_line}}, default: strata, (horizontal lines are grey50, vertical ones are the same colour as the respective strata)
+#' @param alpha numeric value between 0 and 1 as described in the aesthetics of ggplot2 \code{\link[ggplot2]{geom_line}}, default: 1
 #' @param ... other arguments passed on to the method to modify \code{\link[ggplot2]{geom_line}}
 #'
 #' @rdname add_quantiles
@@ -56,6 +56,20 @@ add_quantiles.ggsurvfit <- function(gg,
     
     stop("Invalid argument for `quantiles`, must be numeric. Setting it to 0.5.")
     quantiles <- 0.5
+    
+  }
+  
+  if (!is.character(linetype)) {
+    
+    stop("Invalid argument for `linetype`, must be a character string. Setting it to default 'mixed'.")
+    linetype <- "mixed"
+    
+  }
+  
+  if (!is.character(linecolour)) {
+    
+    stop("Invalid argument for `linecolour`, must be a character string. Setting it to default 'strata'.")
+    linecolour <- "strata"
     
   }
   
@@ -80,7 +94,7 @@ add_quantiles.ggsurvfit <- function(gg,
     dplyr::rename(quantile = name) %>%
     dplyr::mutate(group = paste(strata, quantile, sep = "_")) %>%
     dplyr::rename(n = value) %>%
-    dplyr::mutate(surv = 1-(as.numeric(quantile))/100) %>%
+    dplyr::mutate(surv = 1 - (as.numeric(quantile))/100) %>%
     dplyr::select(-quantity)
   
   horizontal_helper <- model_quantiles_long %>%
@@ -100,7 +114,6 @@ add_quantiles.ggsurvfit <- function(gg,
     linetype_vertical <- linetype
     
   }
-  
   
   if (linecolour == "strata") {
     
