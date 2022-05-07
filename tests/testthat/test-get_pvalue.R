99d3e712f95a2274ade20992715d65e913b2a31b
Daniel Sjoberg <danield.sjoberg@gmail.com>
2022-05-06 22:11:22 -0400

    Update test-get_pvalue.R

--- tests/testthat/test-get_pvalue.R
+++ tests/testthat/test-get_pvalue.R
@@ -38,6 +38,24 @@ testthat::test_that("T1.1. No error when a `survfit` object is passed to the fun
 
   survfit_object <- visR::estimate_KM(adtte, strata = "TRTA")
   testthat::expect_error(visR::get_pvalue(survfit_object), NA)
+
+  # testing the p-value is correct when filtered data is piped to estimate_KM()
+  survfit_p <-
+    adtte %>%
+    dplyr::filter(SEX == "F", AGE < 60) %>%
+    visR::estimate_KM(strata = "TRTA") %>%
+    get_pvalue(ptype = "Log-Rank") %>%
+    dplyr::pull(`p-value`)
+  survdiff_p <-
+    survival::survdiff(
+      survival::Surv(AVAL, 1 - CNSR) ~ TRTA,
+      data =
+        adtte %>%
+        dplyr::filter(SEX == "F", AGE < 60)
+    ) %>%
+    {pchisq(.$chisq, length(.$n) - 1, lower.tail = FALSE)} %>%
+    visR:::.pvalformat()
+  expect_equal(survfit_p, survdiff_p)
 })
 
 testthat::test_that("TT1.2 An error when a `survfit` object is passed to the function with 1 strata", {
@@ -60,7 +78,7 @@ testthat::context("get_pvalue - T2. The functions tests the null hypothesis of n
 testthat::test_that("T2.1 The function supports the Log-Rank test by setting ptype = 'Log-Rank'", {
 
   survfit_object <- visR::estimate_KM(adtte, strata = "TRTA")
-  testthat::expect_equal(visR::get_pvalue(survfit_object, ptype = "Log-Rank"), 
+  testthat::expect_equal(visR::get_pvalue(survfit_object, ptype = "Log-Rank"),
                          get_pvalue_ref1)
 })
 
@@ -81,24 +99,24 @@ testthat::test_that("T2.3 The function supports the Tarone-Ware test by setting 
 testthat::test_that("T2.4 The function calculates the default ptype when ptype = 'All'", {
 
   survfit_object <- visR::estimate_KM(adtte, strata = "TRTA")
-  testthat::expect_equal(visR::get_pvalue(survfit_object, ptype = 'All'), 
+  testthat::expect_equal(visR::get_pvalue(survfit_object, ptype = 'All'),
                          get_pvalue_ref[1:3,])
 })
 
 testthat::test_that("T2.5 The function supports the use of a custom `rho` in the calculation", {
 
   survfit_object <- visR::estimate_KM(adtte, strata = "TRTA")
-  testthat::expect_equal(visR::get_pvalue(survfit_object, 
+  testthat::expect_equal(visR::get_pvalue(survfit_object,
                                           ptype = "Custom",
-                                          rho = 2.4), 
+                                          rho = 2.4),
                          get_pvalue_ref4)
 })
 
 testthat::test_that("T2.6 The function supports the use of a custom `rho` in the calculation when ptype = `All`", {
 
   survfit_object <- visR::estimate_KM(adtte, strata = "TRTA")
-  testthat::expect_equal(visR::get_pvalue(survfit_object, 
-                                          ptype = "All", 
+  testthat::expect_equal(visR::get_pvalue(survfit_object,
+                                          ptype = "All",
                                           rho = 2.4),
                          get_pvalue_ref)
 })
@@ -107,10 +125,10 @@ testthat::test_that("T2.7 The function accepts a vector for ptype, containing mu
 
   survfit_object <- visR::estimate_KM(adtte, strata = "TRTA")
   testthat::expect_equal(visR::get_pvalue(survfit_object,
-                                          ptype = c('Log-Rank', 
-                                                    'Tarone-Ware', 
-                                                    'Custom'), 
-                                          rho = 2.4), 
+                                          ptype = c('Log-Rank',
+                                                    'Tarone-Ware',
+                                                    'Custom'),
+                                          rho = 2.4),
                          get_pvalue_ref134)
 })
 
@@ -161,39 +179,39 @@ testthat::context("get_pvalue - T4. The output object provides the requested inf
 testthat::test_that("T4.1 The output object is a data.frame",{
 
   survfit_object <- visR::estimate_KM(adtte, strata = "TRTA")
-  testthat::expect_identical(class(visR::get_pvalue(survfit_object, 
-                                                    statlist = "test")), 
+  testthat::expect_identical(class(visR::get_pvalue(survfit_object,
+                                                    statlist = "test")),
                              "data.frame")
 })
 
 testthat::test_that("T4.2 The summary statistics are available via the columns of the output object", {
 
   survfit_object <- visR::estimate_KM(adtte, strata = "TRTA")
-  testthat::expect_identical(colnames(visR::get_pvalue(survfit_object, 
-                                                       statlist = c("test", "df"))), 
+  testthat::expect_identical(colnames(visR::get_pvalue(survfit_object,
+                                                       statlist = c("test", "df"))),
                              c("Equality across strata", "df"))
 })
 
 testthat::test_that("T4.3 Each test statistic and associated calculations are available via the rows of the output object", {
 
   survfit_object <- visR::estimate_KM(adtte, strata = "TRTA")
-  testthat::expect_equal(nrow(visR::get_pvalue(survfit_object, 
+  testthat::expect_equal(nrow(visR::get_pvalue(survfit_object,
                                                statlist = c("test", "df"))), 3)
 })
 
 testthat::test_that("T4.4 The statistical tests are ordered in line with the order defined in ptype", {
 
   survfit_object <- visR::estimate_KM(adtte, strata = "TRTA")
-  testthat::expect_identical(visR::get_pvalue(survfit_object, 
-                                              ptype = c("Wilcoxon", "Log-Rank"))[,1], 
+  testthat::expect_identical(visR::get_pvalue(survfit_object,
+                                              ptype = c("Wilcoxon", "Log-Rank"))[,1],
                              c("Wilcoxon", "Log-Rank"))
 })
 
 testthat::test_that("T4.5 The associated calculations of the statistical tests are ordered in line with the order defined in the statlist", {
 
   survfit_object <- visR::estimate_KM(adtte, strata = "TRTA")
-  testthat::expect_identical(colnames(visR::get_pvalue(survfit_object, 
-                                                       statlist = c("df", "test"))), 
+  testthat::expect_identical(colnames(visR::get_pvalue(survfit_object,
+                                                       statlist = c("df", "test"))),
                              c("df", "Equality across strata"))
 })
 
@@ -201,18 +219,18 @@ testthat::test_that("T4.6 The Chisq statistic has the same precision as the pval
 
   survfit_object <- visR::estimate_KM(adtte, strata = "TRTA")
   totest <- visR::get_pvalue(survfit_object, statlist = c("Chisq", "pvalue"))
-  
+
   pvals_nchar_after_dot <- gsub(".+?\\.", "", totest[["p-value"]]) %>% nchar()
   chisq_nchar_after_dot <- gsub(".+?\\.", "", totest[["Chisq"]]) %>% nchar()
-  
+
   testthat::expect_identical(pvals_nchar_after_dot, chisq_nchar_after_dot)
-  
+
   survfit_object <- visR::estimate_KM(adtte, strata = "SEX")
   totest <- visR::get_pvalue(survfit_object, statlist = c("Chisq", "pvalue"))
-  
+
   pvals_nchar_after_dot <- gsub(".+?\\.", "", totest[["p-value"]]) %>% nchar()
   chisq_nchar_after_dot <- gsub(".+?\\.", "", totest[["Chisq"]]) %>% nchar()
-  
+
   testthat::expect_identical(pvals_nchar_after_dot, chisq_nchar_after_dot)
 })
 
