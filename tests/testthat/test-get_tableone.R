44982da383a998c03af0b485ca3e87a2fb5ed0af
ardeeshany <ardeeshany@gmail.com>
2022-05-07 17:05:08 -0400

    Add na.rm option to remove the strata variable in rows

--- tests/testthat/test-get_tableone.R
+++ tests/testthat/test-get_tableone.R
@@ -24,6 +24,9 @@
 #' T4.3 An error when the `summary_function` is a function not build for it
 #' T4.4 An error when the `summary_function` is `summarize_long`
 #' T4.5 No error when the `summary_function` is `summarize_short`
+#' T5. The tableone removes strata variables in rows that leads NA values
+#' T5.1 An error when the the table includes the strata variables if `na.rm = TRUE`
+#' An error when the the table does not include any of the strata variables if `na.rm = FALSE`
 
 # Requirement T1 ----------------------------------------------------------
 
@@ -39,7 +42,7 @@ testthat::test_that("T1.1. No error when `data` is of class `data.frame`", {
 testthat::test_that("T1.2. No error when `data` is of class `tibble`", {
 
   data <- dplyr::as_tibble(adtte)
-  
+
   testthat::expect_error(visR::get_tableone(data = data), NA)
 
 })
@@ -47,10 +50,10 @@ testthat::test_that("T1.2. No error when `data` is of class `tibble`", {
 testthat::test_that("T1.3 No error when `data` is of class `data.table`", {
 
   if (nzchar(find.package("data.table"))) {
-    
+
     data <- data.table::as.data.table(adtte)
     testthat::expect_error(visR::get_tableone(data = data), NA)
-    
+
   }
 })
 
@@ -85,7 +88,7 @@ testthat::test_that("T2.2 An error when `strata` is a string that is not a colna
 
 testthat::test_that("T2.3 Additional colnames in the tableone are the `strata` values (for one `strata`)", {
 
-  trtp_colnames <- colnames(visR::get_tableone(data = adtte, 
+  trtp_colnames <- colnames(visR::get_tableone(data = adtte,
                                                strata = c("TRTP")))[4:6]
   testthat::expect_equal(trtp_colnames, levels(adtte$TRTP))
 
@@ -93,10 +96,10 @@ testthat::test_that("T2.3 Additional colnames in the tableone are the `strata` v
 
 testthat::test_that("T2.4 Additional colnames in the tableone are the crossproduct of all `strata` values (for more than one `strata`)", {
 
-  mapply_colnames <- c(mapply(function(x, y) paste(x, y, sep = "_"), 
+  mapply_colnames <- c(mapply(function(x, y) paste(x, y, sep = "_"),
                             levels(adtte$TRTP),
                             MoreArgs = list(levels(adtte$SEX))))
-  visR_colnames <- colnames(visR::get_tableone(data = adtte, 
+  visR_colnames <- colnames(visR::get_tableone(data = adtte,
                                                strata = c("TRTP", "SEX")))[4:9]
   testthat::expect_equal(mapply_colnames, visR_colnames)
 
@@ -107,30 +110,30 @@ testthat::test_that("T2.4 Additional colnames in the tableone are the crossprodu
 testthat::context("get_tableone - T3. The tableone includes expected columnnames")
 
 testthat::test_that("T3.1 Tableone by default includes columns `variable`, `statistic`, and `Total`", {
-  
+
   tableone_colnames <- colnames(visR::get_tableone(data = adtte))
-  
+
   testthat::expect_equal(tableone_colnames, c("variable", "statistic", "Total"))
 
 })
 
 testthat::test_that("T3.2 Tableone still includes the colum `Total` if `overall` is FALSE but no `strata` is given", {
-  
+
   tableone_colnames <- colnames(visR::get_tableone(data = adtte, overall = FALSE))
-  
+
   testthat::expect_equal(tableone_colnames, c("variable", "statistic", "Total"))
 
 })
 
 testthat::test_that("T3.3 Tableone does not include the colum `Total` if `overall` is FALSE and a `strata` is given", {
 
-  tblone_colnames <- colnames(visR::get_tableone(data = adtte, 
-                                                 overall = FALSE, 
+  tblone_colnames <- colnames(visR::get_tableone(data = adtte,
+                                                 overall = FALSE,
                                                  strata = c("TRTP")))
-  
-  testthat::expect_equal(tblone_colnames, 
+
+  testthat::expect_equal(tblone_colnames,
                          c("variable", "statistic", levels(adtte$TRTP)))
-  
+
 })
 
 # Requirement T4 ---------------------------------------------------------------
@@ -139,28 +142,28 @@ testthat::context("get_tableone - T4. The function only accepts suitable summary
 
 testthat::test_that("T4.1 An error when the `summary_function` is NULL", {
 
-  testthat::expect_error(visR::get_tableone(data = adtte, 
+  testthat::expect_error(visR::get_tableone(data = adtte,
                                             summary_function = NULL))
 
 })
 
 testthat::test_that("T4.2 An error when the `summary_function` is a string", {
 
-  testthat::expect_error(visR::get_tableone(data = adtte, 
+  testthat::expect_error(visR::get_tableone(data = adtte,
                                             summary_function = "A"))
 
 })
 
 testthat::test_that("T4.3 An error when the `summary_function` is a function not build for it", {
 
-  testthat::expect_error(visR::get_tableone(data = adtte, 
+  testthat::expect_error(visR::get_tableone(data = adtte,
                                             summary_function = sum))
 
 })
 
 testthat::test_that("T4.4 An error when the `summary_function` is `summarize_long`", {
 
-  testthat::expect_error(visR::get_tableone(data = adtte, 
+  testthat::expect_error(visR::get_tableone(data = adtte,
                                             summary_function = summarize_long))
 
 })
@@ -168,8 +171,41 @@ testthat::test_that("T4.4 An error when the `summary_function` is `summarize_lon
 
 testthat::test_that("T4.5 No error when the `summary_function` is `summarize_short`", {
 
-  testthat::expect_error(visR::get_tableone(data = adtte, 
+  testthat::expect_error(visR::get_tableone(data = adtte,
                                             summary_function = summarize_short), NA)
+
+})
+
+# Requirement T5 ---------------------------------------------------------------
+
+testthat::context("get_tableone - T5. The tableone removes strata variables in rows that leads NA values")
+
+testthat::test_that("T5.1 An error when the the table includes the strata variables if `na.rm = TRUE`", {
+
+  strata <- c("SEX", "RACE")
+
+  table <- adtte %>%
+    dplyr::filter(SAFFL == "Y") %>%
+    dplyr::select(RACE,SEX, AGEGR1) %>%
+    visR::get_tableone(strata = strata,
+                       na.rm = TRUE)
+
+  testthat::expect_true(sum(strata %in% unique(table$variable)) == 0)
+
+})
+
+
+testthat::test_that("T5.2 An error when the the table does not include any of the strata variables if `na.rm = FALSE`", {
+
+  strata <- c("SEX", "RACE")
+
+  table <- adtte %>%
+    dplyr::filter(SAFFL == "Y") %>%
+    dplyr::select(RACE,SEX, AGEGR1) %>%
+    visR::get_tableone(strata = strata,
+                       na.rm = FALSE)
+
+  testthat::expect_true(sum(strata %in% unique(table$variable)) == length(strata))
 
 })
 
